919c0a5f68c9373d49742965a6103969b235c0f8
AlexAvVilla <alex.avila3232@gmail.com>
2023-02-20 01:32:06 -0600

    Se agrega el boton entrega de equipo vaya ahora si

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
 
+import Controllador.entregasController;
 import Controllador.equiposController;
+import Modelos.entregaModel;
 import Modelos.equipoModel;
+import Vistas.frmEntrega;
 import Vistas.frmPrincipal;
 import Vistas.frmEquipo;
 
@@ -11,9 +14,13 @@ public class main {
         frmPrincipal VistaPrincipal = new frmPrincipal();
         frmEquipo VistaEquipo = new frmEquipo();
         equipoModel ModeloEquipo = new equipoModel();
+        frmEntrega VistaEntrega = new frmEntrega();
+        entregaModel ModeloEntrega = new entregaModel();
         
         equiposController ControladorUsuarios = new equiposController(VistaPrincipal,
                 VistaEquipo, ModeloEquipo);
+        
+        entregasController ControladorEntregas = new entregasController(VistaPrincipal, VistaEntrega, ModeloEntrega);
     }
     
 }
